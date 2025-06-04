5a1583b88f3715b61487ea6193f504b2bdf8a793
yashrajvrma <vermayashraj2005@gmail.com>
2025-06-04 15:34:02 +0530

    added save feature

--- app/providers.tsx
+++ app/providers.tsx
@@ -5,30 +5,26 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider as ReduxProvider } from "react-redux";
-import { store } from "@/lib/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
   return (
     <SessionProvider>
-      <ReduxProvider store={store}>
-        <QueryClientProvider client={queryClient}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <TooltipProvider>{children}</TooltipProvider>
-            <Toaster
-              toastOptions={{
-                className: "font-sans",
-              }}
-            />
-          </ThemeProvider>
-        </QueryClientProvider>
-      </ReduxProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <TooltipProvider>{children}</TooltipProvider>
+          <Toaster
+            toastOptions={{
+              className: "font-sans",
+            }}
+          />
+        </ThemeProvider>
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
--- components/button/saveButton.tsx
+++ components/button/saveButton.tsx
@@ -1,13 +1,50 @@
 "use client";
 
+import { useEditorContent, useEditorStore } from "@/lib/store/editor";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+type UpdatedDocsResponse = {
+  id: string | null;
+  updatedDocJson: object;
+};
+
+const saveDocsFn = ({ id, updatedDocJson }: UpdatedDocsResponse) => {
+  return axios.post(`/api/v1/save-docx`, { id, updatedDocJson });
+};
+
 export default function SaveButton() {
-  const handleSave = async () => {};
+  const editor = useEditorStore((state) => state.editor);
+  const id = useEditorContent((state) => state.id);
+
+  const queryClient = useQueryClient();
+
+  const { mutate: saveDocs, isPending } = useMutation({
+    mutationFn: saveDocsFn,
+    onSuccess: () => {
+      toast.success("Docs saved successfully");
+      queryClient.invalidateQueries({
+        queryKey: ["thread"],
+      });
+    },
+  });
+
+  const handleSave = async () => {
+    if (!editor) {
+      console.log("Editor doesn't exist");
+      return;
+    }
+    const updatedDocJson = editor?.getJSON();
+    saveDocs({ id, updatedDocJson });
+  };
+
   return (
     <button
       onClick={() => handleSave()}
       className="flex justify-center items-center text-sm px-2.5 py-2 text-foreground rounded-lg bg-primary-foreground hover:bg-muted-foreground/20"
     >
-      Save
+      {isPending ? "Saving" : "Save"}
     </button>
   );
 }
