1cdcbf8a987b786555ffdb90d7a28a5aee9248a2
yashrajvrma <vermayashraj2005@gmail.com>
2025-06-19 04:36:24 +0530

    fix prisma instance error

--- db/index.ts
+++ db/index.ts
@@ -1,32 +1,32 @@
-// import { PrismaClient } from "../node_modules/prisma/prisma-client";
-
-// const prismaClientSingleton = () => {
-//   return new PrismaClient();
-// };
-
-// type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
-
-// // eslint-disable-next-line
-// const globalForPrisma = globalThis as unknown as {
-//   prisma: PrismaClientSingleton | undefined;
-// };
-
-// const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-// export default prisma;
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 import { PrismaClient } from "../node_modules/prisma/prisma-client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
 };
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// eslint-disable-next-line
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
+export default prisma;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
-export default prisma;
+// import { PrismaClient } from "../node_modules/prisma/prisma-client";
+// import { withAccelerate } from "@prisma/extension-accelerate";
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient;
+// };
+
+// const prisma =
+//   globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
