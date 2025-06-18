e8b666f635d619db359b33eaac3a7de1aedc4904
yashrajvrma <vermayashraj2005@gmail.com>
2025-06-18 15:38:16 +0530

    fixed twitter og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import localFont from "next/font/local";
 import { Providers } from "./providers";
 import { Instrument_Serif } from "next/font/google";
 
+const baseURL = "https://breezeai.live";
+
 const instrumentSerif = Instrument_Serif({
   variable: "--font-instrument-serif",
   subsets: ["latin"],
@@ -26,7 +28,7 @@ const garamondBookCond = localFont({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://breezeai.live"),
+  metadataBase: new URL(baseURL),
   keywords: [
     "breeze",
     "word",
@@ -50,16 +52,27 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
+    title: "Breeze",
+    description:
+      "AI powered document editor that let's you create word docs in seconds.",
+    images: [
+      {
+        url: `${baseURL}/assets/images/og-img.png`,
+        width: 1200,
+        height: 630,
+        alt: "Breeze AI Document Editor Preview",
+      },
+    ],
   },
   openGraph: {
     title: "Breeze",
-    url: "https://breezeai.live",
+    url: baseURL,
     type: "website",
     description:
       "AI powered document editor that let's you create word docs in seconds.",
     images: [
       {
-        url: "/assets/images/og-img.png",
+        url: `${baseURL}/assets/images/og-img.png`,
         width: 1200,
         height: 630,
         alt: "Breeze AI Document Editor Preview",
