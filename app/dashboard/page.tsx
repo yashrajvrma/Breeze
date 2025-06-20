92a150996e0fa5df2853b2b0d5218d5b02528843
yashrajvrma <vermayashraj2005@gmail.com>
2025-06-20 20:32:54 +0530

    fixed accounts and settings

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,13 +19,13 @@ export default function Page() {
       <AppSidebar />
       <SidebarInset>
         <header className="flex h-14 shrink-0 items-center gap-2">
-          <div className="flex flex-1 items-center gap-2 px-3">
+          <div className="flex flex-1 items-center gap-1 px-3">
             <SidebarTrigger />
             <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
               <BreadcrumbList>
                 <BreadcrumbItem>
-                  <BreadcrumbPage className="line-clamp-1">
+                  <BreadcrumbPage className="line-clamp-1 font-sans">
                     Project Management & Task Tracking
                   </BreadcrumbPage>
                 </BreadcrumbItem>
--- components/nav-actions.tsx
+++ components/nav-actions.tsx
@@ -13,10 +13,12 @@ import {
   LineChart,
   Link,
   MoreHorizontal,
+  Router,
   Settings2,
   Star,
   Trash,
   Trash2,
+  User,
 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -33,121 +35,152 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  SidebarProvider,
 } from "@/components/ui/sidebar";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { useSession } from "next-auth/react";
+import {
+  CircleUser,
+  EllipsisVerticalIcon,
+  MessageCircle,
+  Paintbrush,
+  Settings,
+} from "lucide-react";
+import { Skeleton } from "./ui/skeleton";
+import LoginButton from "./button/loginButton";
+import { ThemeSettings } from "./theme-settings";
+import { ProfileSettings } from "./profile-settings";
+import { AccountSettings } from "./account-settings";
+import { ContactUsSettings } from "./contact-us-settings";
+import { cn } from "@/lib/utils";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "./ui/breadcrumb";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { useRouter } from "next/navigation";
 
-const data = [
-  [
-    {
-      label: "Customize Page",
-      icon: Settings2,
-    },
-    {
-      label: "Turn into wiki",
-      icon: FileText,
-    },
+const data = {
+  nav: [
+    { name: "Profile", icon: CircleUser, component: ProfileSettings },
+    { name: "Appearance", icon: Paintbrush, component: ThemeSettings },
+    { name: "Account", icon: Settings, component: AccountSettings },
+    { name: "Contact Us", icon: MessageCircle, component: ContactUsSettings },
   ],
-  [
-    {
-      label: "Copy Link",
-      icon: Link,
-    },
-    {
-      label: "Duplicate",
-      icon: Copy,
-    },
-    {
-      label: "Move to",
-      icon: CornerUpRight,
-    },
-    {
-      label: "Move to Trash",
-      icon: Trash2,
-    },
-  ],
-  [
-    {
-      label: "Undo",
-      icon: CornerUpLeft,
-    },
-    {
-      label: "View analytics",
-      icon: LineChart,
-    },
-    {
-      label: "Version History",
-      icon: GalleryVerticalEnd,
-    },
-    {
-      label: "Show delete pages",
-      icon: Trash,
-    },
-    {
-      label: "Notifications",
-      icon: Bell,
-    },
-  ],
-  [
-    {
-      label: "Import",
-      icon: ArrowUp,
-    },
-    {
-      label: "Export",
-      icon: ArrowDown,
-    },
-  ],
-];
-
+};
 export function NavActions() {
   const [isOpen, setIsOpen] = React.useState(false);
+  const router = useRouter();
 
-  React.useEffect(() => {
-    setIsOpen(false);
-  }, []);
+  const [selectedItem, setSelectedItem] = React.useState("Profile");
+  const { data: session, status } = useSession();
+
+  const CurrentComponent = React.useMemo(() => {
+    const item = data.nav.find((navItem) => navItem.name === selectedItem);
+    return item ? item.component : null;
+  }, [selectedItem]);
+
+  if (status === "loading") {
+    return (
+      <div className="flex justify-center px-3 py-2">
+        <Skeleton className="flex items-center h-16 w-full" />
+      </div>
+    );
+  }
+  if (!session) {
+    router.push("/signin");
+  }
 
   return (
     <div className="flex items-center gap-2 text-sm font-sans">
-      <div className="hidden font-medium text-muted-foreground md:inline-block">
+      <div className="hidden font-medium text-muted-foreground md:inline-block font-sm">
         Edit Oct 08
       </div>
       <Button variant="ghost" size="icon" className="h-7 w-7">
-        <Star />
+        <Star size={16} />
       </Button>
-      <Popover open={isOpen} onOpenChange={setIsOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="h-7 w-7 data-[state=open]:bg-accent"
-          >
-            <MoreHorizontal />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent
-          className="w-56 overflow-hidden rounded-lg p-0"
-          align="end"
-        >
-          <Sidebar collapsible="none" className="bg-transparent">
-            <SidebarContent>
-              {data.map((group, index) => (
-                <SidebarGroup key={index} className="border-b last:border-none">
-                  <SidebarGroupContent className="gap-0">
-                    <SidebarMenu>
-                      {group.map((item, index) => (
-                        <SidebarMenuItem key={index}>
-                          <SidebarMenuButton>
-                            <item.icon /> <span>{item.label}</span>
-                          </SidebarMenuButton>
-                        </SidebarMenuItem>
-                      ))}
-                    </SidebarMenu>
-                  </SidebarGroupContent>
-                </SidebarGroup>
-              ))}
-            </SidebarContent>
-          </Sidebar>
-        </PopoverContent>
-      </Popover>
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger asChild>
+          <Avatar className="h-7 w-7 rounded-lg">
+            <AvatarImage
+              src={session?.user?.image || ""}
+              referrerPolicy="no-referrer"
+              alt="User"
+            />
+            <AvatarFallback>
+              <User className="h-4 w-4" />
+            </AvatarFallback>
+          </Avatar>
+        </DialogTrigger>
+        <DialogContent className="overflow-hidden p-0 font-sans max-h-[90vh] w-[95vw] max-w-[95vw] md:max-h-[500px] md:max-w-[700px] lg:max-w-[800px] rounded-xl">
+          <DialogTitle className="sr-only">Settings</DialogTitle>
+          <SidebarProvider className="items-start border">
+            <div className="flex flex-col md:flex-row w-full">
+              <Sidebar
+                collapsible="none"
+                className="flex bg-background border-r md:border-b-0 border-b w-full h-16 md:w-auto md:h-auto overflow-y-hidden"
+              >
+                <SidebarContent className="flex items-center justify-center md:items-center md:justify-start overflow-y-hidden w-full">
+                  <SidebarGroup className="overflow-y-hidden w-full ">
+                    <SidebarGroupContent className="overflow-y-hidden w-full ">
+                      <SidebarMenu className="flex flex-row md:flex-col px-2 md:px-3 py-2 md:py-3 gap-x-2 md:gap-x-0 gap-y-0 md:gap-y-2 items-center justify-center md:items-center md:justify-center overflow-x-auto md:overflow-x-visible overflow-y-hidden w-full">
+                        {data.nav.map((item) => (
+                          <SidebarMenuItem
+                            key={item.name}
+                            className="flex md:w-full"
+                          >
+                            <SidebarMenuButton
+                              isActive={item.name === selectedItem}
+                              className={cn(
+                                "flex hover:bg-muted-foreground/10  justify-center items-center align-middle",
+                                item.name === selectedItem &&
+                                  "bg-muted-foreground/10 hover:bg-muted-foreground/10"
+                              )}
+                              onClick={() => {
+                                console.log("selected", item.name);
+                                setSelectedItem(item.name);
+                              }}
+                            >
+                              <item.icon
+                                size={16}
+                                className="flex shrink-0 items-center align-middle"
+                              />
+                              <span className="hidden md:inline w-full">
+                                {item.name}
+                              </span>
+                            </SidebarMenuButton>
+                          </SidebarMenuItem>
+                        ))}
+                      </SidebarMenu>
+                    </SidebarGroupContent>
+                  </SidebarGroup>
+                </SidebarContent>
+              </Sidebar>
+              <main className="flex h-[350px] md:h-[480px] flex-1 flex-col overflow-hidden bg-muted-foreground/10 min-w-0">
+                <header className="flex h-12 md:h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+                  <div className="flex items-center gap-2 px-2 md:px-4">
+                    <Breadcrumb>
+                      <BreadcrumbList>
+                        <BreadcrumbItem className="px-1 md:px-2">
+                          <BreadcrumbPage className="font-semibold text-base md:text-lg">
+                            {selectedItem}
+                          </BreadcrumbPage>
+                        </BreadcrumbItem>
+                      </BreadcrumbList>
+                    </Breadcrumb>
+                  </div>
+                </header>
+
+                <div className="flex flex-1 flex-col gap-4 px-2 py-0 md:px-4 md:py-0 pt-0 min-h-[380px] overflow-auto ">
+                  {CurrentComponent && <CurrentComponent />}
+                </div>
+              </main>
+            </div>
+          </SidebarProvider>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
